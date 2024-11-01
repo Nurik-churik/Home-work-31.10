cca2ce4f05c1eef36711ab5fc89077d60336464d
Nurzat Amirzhan <zhanna910411@gmail.com>
2024-11-01 10:59:52 +0300

    home work 31.10

---
+++ home work 31.10/index.js
@@ -0,0 +1,6 @@
+var slicesPerPizza = 8 //Әр пицца каропкасындағы пицца саны
+var numberOfPizza = 5 // сол каропканың саны
+
+total = (slicesPerPizza * numberOfPizza) 
+
+console.log(total) //Алекс кешке 40 тілім пицца бар екенін білді
